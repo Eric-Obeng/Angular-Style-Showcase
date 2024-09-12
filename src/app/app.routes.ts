f057ab81ec02eb91ca19e59adf08ce27a5352213
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-12 15:29:14 +0000

    routing for lazy loading updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HeaderComponent,
+    children: [
+      {
+        path: 'main',
+        loadComponent: () =>
+          import('./component/main/main.component').then(
+            (m) => m.MainComponent
+          ),
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      },
+    ],
+  },
+  {
+    path: '',
+    component: FooterComponent,
+  },
+];
