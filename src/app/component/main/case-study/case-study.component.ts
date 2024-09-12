01515d177b75eb51e843ab08b136672e489b3fca
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-12 15:30:44 +0000

    case study component created

--- src/app/component/main/case-study/case-study.component.ts
+++ src/app/component/main/case-study/case-study.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,8 +6,23 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './case-study.component.html',
-  styleUrl: './case-study.component.scss'
+  styleUrl: './case-study.component.scss',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(20px)' }),
+        animate(
+          '0.5s 0.3s ease-out',
+          style({ opacity: 1, transform: 'translateY(0)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class CaseStudyComponent {
+  visible = false;
 
+  onvisibile(isVisible: boolean) {
+    this.visible = isVisible;
+  }
 }
