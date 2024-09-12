e05af7b25d35fcebd54966f597381929fdcea0be
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-12 15:30:22 +0000

    main component updated to handle layout

--- src/app/component/main/main.component.ts
+++ src/app/component/main/main.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { OurServicesComponent } from "./our-services/our-services.component";
-import { CaseStudyComponent } from "./case-study/case-study.component";
+import { CommonModule } from '@angular/common';
+import { OurServicesComponent } from './our-services/our-services.component';
+import { CaseStudyComponent } from './case-study/case-study.component';
+import { IntersectionObserverDirective } from '../../intersection-observer.directive';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [OurServicesComponent, CaseStudyComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrls: ['./main.component.scss'],
+  imports: [
+    CommonModule,
+    OurServicesComponent,
+    CaseStudyComponent,
+    IntersectionObserverDirective,
+  ],
 })
 export class MainComponent {
+  ourServicesVisible = false;
+  caseStudyVisible = false;
 
+  onOurServicesVisible(isVisible: boolean): void {
+    this.ourServicesVisible = isVisible;
+  }
+
+  onCaseStudyVisible(isVisible: boolean): void {
+    this.caseStudyVisible = isVisible;
+  }
 }
